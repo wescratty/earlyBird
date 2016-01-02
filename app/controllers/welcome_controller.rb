02a3feb5aec9f16ac4239f74cf1034a4c55635f3
wescratty <wescratty@gmail.com>
2016-01-01 23:00:44 -0700

    experimenting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   def index
   end
   def sample
-  	@controller_massage = "Hello from controller"
+  	@controller_massage = "Hello from cont"
   end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
 module WelcomeHelper
+	def add_name (nameArray, a)
+		if(!nameArray.include?(a))
+			nameArray.push(a)
+ 		end
+ 	return nameArray
+	
+end
 end
