3e5dd108be58474de0904229a88520a766836ed1
wescratty <wescratty@gmail.com>
2016-01-12 16:34:50 -0700

    added additional regex

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -4,6 +4,20 @@ module WelcomeHelper
 			nameArray.push(a)
  		end
  	return nameArray
-	
+ end
+
+ 	def match_indexes(string, regex)
+  matches = string.match(regex)
+
+if(matches)
+  (1...matches.length).map do |index|
+    [matches.begin(index), matches.end(index) - 1]
 end
+else
+	return (matches.to_s +"  :no match")
+  end
+end
+
+	
+
 end
